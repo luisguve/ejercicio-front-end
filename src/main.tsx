a4a937b849c8ae619e108aeec2ff12f2a9bbfd54
anydayr <annyrondon491@gmail.com>
2023-11-19 10:54:36 -0400

    feat: tanstack query, vite and ts config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import './style/index.scss'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import queryClient from './queryClient.ts'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
 )
